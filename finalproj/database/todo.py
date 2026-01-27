cd5e08e946a5769d3dcfa1ebac29cfc182352c99
Nihar Deshpande <85449555+nihardeshpande@users.noreply.github.com>
2026-01-27 11:29:31 +0530

    Add mark_completed function to update task status

--- finalproj/database/todo.py
+++ finalproj/database/todo.py
@@ -11,3 +11,11 @@ def get_tasks(conn, userID):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM todo_tasks WHERE userID=?", (userID,))
     return cursor.fetchall()
+
+def mark_completed(conn, taskID):
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE todo_tasks SET status='Completed' WHERE todoTaskID=?",
+        (taskID,)
+    )
+    conn.commit()
